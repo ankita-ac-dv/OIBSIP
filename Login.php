dfea8537e16dbbf1fd360c6a68ab60185ced6d4c
Ankita Kumari <114810670+ankitakri963@users.noreply.github.com>
2023-03-05 11:38:23 +0530

    Add files via upload

--- Login.php
+++ Login.php
@@ -1,51 +1,56 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-</head>
-<body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
     <?php
-        include 'Header.php';
+    // Start the session for user login.
+      session_start();
+    // Check user click on login button.
+      if (isset($_POST['login'])) {
+        // Connect database.
+        include 'dbcon.php';
+        // Set variable for Username and password.
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        // Fatch the record into the database.
+        $sql = "SELECT * FROM registration WHERE Email ='$username' AND Password='$password'";
+        // Execute the query.
+        $r = mysqli_query($conn, $sql);
+        // Fatch the quesry.
+        $row = mysqli_fetch_assoc($r);
+        // Check email and password is same as it is in the database.
+        if ($row['Email'] === $username && $row['Password'] === $password) {
+              header("Location: Dashboard.php");
+            exit();
+          }
+          else{
+            echo "<script>alert('Please enter username and password.');</script>";
+          }
+        }
     ?>
-    <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-                    <!-- <div class="lg:w-2/3 md:w-1/2 bg-gray-100 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                        <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1861.51387455906!2d85.23146787400663!3d26.08571938465402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjbCsDA1JzA4LjQiTiA4NcKwMTMnNTIuMCJF!5e0!3m2!1sen!2sin!4v1676857363086!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
-                            <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-                                <div class="lg:w-1/2 px-6">
-                                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-                                <p class="mt-1">Vill+PO+PS - Karja, Muzaffarpur-843143, Bihar.</p>
-                                </div>
-                                <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                                <a class="text-indigo-500 leading-relaxed">anshu.ac.dv@gmail.com</a>
-                                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-                                <p class="leading-relaxed">+91 8710080479</p>
-                            </div>
-                        </div>
-                    </div> -->
-                    <div class="lg:w-2/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-2 md:py-8 mt-8 md:mt-0 rounded-lg shadow-lg p-5">
-                        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Send a Message</h2>
-                        <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-                    <div class="relative mb-4">
-                        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-                        <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                    </div>
-                    <div class="relative mb-4">
-                        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                    </div>
-                    <div class="relative mb-4">
-                        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-                        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                    </div>
-                    <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send</button>
-                    </div>
-            </div>
-        </section>
-</body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <div class="container">
+      <form class="p-5 shadow bg-info" action="Login.php" method="post">
+        <center><h3>Login Now</h3></center>
+        <div class="row g-3">
+          <div class="col mb-3 mt-5">
+            <label class="form-label">Enter Email</label>
+            <input type="email" class="form-control" name="username">
+          </div>
+          <div class="col mb-3 mt-5">
+            <label class="form-label">Enter Password</label>
+            <input type="password" class="form-control" name="password">
+          </div>
+        </div>
+        <button type="submit" name="login" class="btn btn-primary">Login Now</button><br><br>
+        <p>If you do not have any account then <a href="Register.php"> register</a> yourself.</p>
+      </form>
+    </div>
+  </body>
 </html>
